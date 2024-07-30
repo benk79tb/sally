9f70cdbffb07bb0157298920a6c0d3bf39bd5a0f
Benjamin Keller <bkeller79@hotmail.com>
2024-07-30 08:44:36 +0200

    Debug

--- src/sally/core/serving.py
+++ src/sally/core/serving.py
@@ -49,9 +49,10 @@ def setup(hby, *, alias, httpPort, hook, auth, listen=False, timeout=10, retry=3
 
     print(f"Using hab {hab.name}:{hab.pre}")
 
-    print("Keys and values of hab.kever.serder:")
-    for key, value in hab.kever.serder.items():
-        print(f"\t{key}: {value}")
+    print("Attributes of hab.kever.serder:")
+    for attr in dir(hab.kever.serder):
+        if not attr.startswith('_'):
+            print(f"\t{attr}: {getattr(hab.kever.serder, attr)}")
     # print(f"\tCESR Qualifed Base64 Public Key:  {hab.kever.serder.verfers[0].qb64}")
     # print(f"\tPlain Base64 Public Key:          {encodeB64(hab.kever.serder.verfers[0].raw).decode('utf-8')}")
     mbx = storing.Mailboxer(name=hby.name)
